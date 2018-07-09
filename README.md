be03fce28e86dc5e5ff4375486490036ed4a29bf
Lab-Zjt <a1322144594@gmail.com>
2018-07-09 20:07:30 +0800

    update

---
+++ README.md
@@ -0,0 +1,45 @@
+# go-ds-gen
+
+## Introdution
+
+Support: Vector, Stack, Queue, Deque, List, Priority Queue, Map
+
+NOT recommend to use iterator because iterator may keep the object alive. If use, please call Illegal() method to avoid this problem.
+
+## Usage
+
+```
+-t type name, like int, float, string and so on
+-c container name, support: Vector, Stack, Queue, Deque, List, Priority Queue, Map
+-g generated name, like IntVector, FloatList or what you like
+-i iterator name, like ItInt, ItString or what you like
+-k key type name, using in Map
+-v value type name, using in Map
+-ft filter function typename
+-mp map function typename
+-rd reduce function typename
+-fe foreach function typename
+```
+
+`./codegen -t string -c PriorityQueue -g StringPQ >> ./StringPQ.go`
+`./codegen -k int -v string -i Iterator -c Map -g ISmap >> ./ISmap.go`
+
+## API
+
+```
+Vector: Size,Empty,Begin,End,PushBack,PopBack,PushFront,PopFront,Front,Back,At,Filter,Map,ForEach,Reduce
+List: Size,Empty,PushBack,PopBack,PushFront,PopFront,Begin,End,Front,Back,Clear,Erase,Find,Filter,Map,ForEach,Reduce
+Deque: Size,Empty,PushBack,PushFront,PopBack,PopFront,Clear
+Queue: Size,Empty,Push,Pop,Front,Back,Clear
+PriorityQueue: Size,Empty,Push,Pop,Top,Size,Clear
+Stack: Size,Empty,Push,Pop,Top,Clear
+Map: Size,Empty,Insert,Erase,Begin,End,At,Filter,Map,ForEach,Reduce,Clear
+```
+
+## Attention
+
+Map, PriorityQueue need compare function `less($typename,$typename)bool`, you need to pass a function when you create a map or priority queue, or you can modify the constructor `New$container` of the generated file.
+
+NOT recommend to use iterator because iterator may keep the object alive. If use, please call Illegal() method to avoid this problem or promise iterator will be destroyed immediately.
+
+Maybe you shoule modify the package name of the generated file.
--- main.go
+++ main.go
@@ -9,20 +9,32 @@ import (
 )
 
 var (
-	_type, _it, _container, _name string
+	_type, _it, _container, _gen, _filter, _map, _reduce, _foreach, _key, _value string
 )
+
+type A struct {
+	a int
+	b string
+}
 
 func init() {
 	flag.StringVar(&_type, "t", "int", "type name")
 	flag.StringVar(&_container, "c", "List", "container name, support List, PriorityQueue, Queue, Stack, Deque now")
-	flag.StringVar(&_name, "g", _type+_container, "generated name")
+	flag.StringVar(&_gen, "g", _type+_container, "generated name")
 	flag.StringVar(&_it, "i", "ListIt", "iterator name")
+	flag.StringVar(&_filter, "ft", _type+"FilterFunc", "filter function name")
+	flag.StringVar(&_map, "mp", _type+"MapFunc", "map function name")
+	flag.StringVar(&_reduce, "rd", _type+"ReduceFunc", "reduce func name")
+	flag.StringVar(&_foreach, "fe", _type+"ForEachFunc", "foreach function name")
+	flag.StringVar(&_key, "k", "string", "key type name")
+	flag.StringVar(&_value, "v", "string", "value type name")
 }
 
+type vector []int
 
 func main() {
 	flag.Parse()
-	f, err := os.OpenFile("/home/zjt/code-repo/go/codegen/template_"+_container+".txt", os.O_RDONLY, 0666)
+	f, err := os.OpenFile("template_"+_container+".txt", os.O_RDONLY, 0666)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -34,6 +46,12 @@ func main() {
 	str := string(buf)
 	str = strings.Replace(str, "{{type}}", _type, -1)
 	str = strings.Replace(str, "{{it}}", _it, -1)
-	str = strings.Replace(str, "{{container}}", _name, -1)
+	str = strings.Replace(str, "{{container}}", _gen, -1)
+	str = strings.Replace(str, "{{key}}", _key, -1)
+	str = strings.Replace(str, "{{value}}", _value, -1)
+	str = strings.Replace(str, "{{mapfunc}}", _map, -1)
+	str = strings.Replace(str, "{{filterfunc}}", _filter, -1)
+	str = strings.Replace(str, "{{foreachfunc}}", _foreach, -1)
+	str = strings.Replace(str, "{{reducefunc}}", _reduce, -1)
 	fmt.Println(str)
 }
